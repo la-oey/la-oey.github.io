2fa879b458a4926e10f1910457048d3b95345cd0
Lauren Oey <loey@ucsd.edu>
2018-11-26 22:31:56 -0800

    changes to JUR publication name and laid groundwork for easter egg on personal page

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function konamicode(){
+	var konamiSequence = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+	var index = 0;
+
+	var saved_keydown = function(e){
+		var keyCode = e.keyCode;
+		if(index == konamiSequence.length - 1 && keyCode == konamiSequence[index]){
+			console.log("website under construction. fun games coming soon.")
+		}
+		else if(keyCode == konamiSequence[index]){
+			index += 1;
+		} else{
+			index = 0;
+			document.onkeydown = null;
+			setTimeout(function(){
+				document.onkeydown = saved_keydown;
+			}, 3000);
+		}
+	}
+	document.onkeydown = saved_keydown;
+}
